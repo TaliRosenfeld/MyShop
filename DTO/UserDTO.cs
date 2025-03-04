286beb72cf2f5b8b1173d00b7c13ac377ca62578
Tali Rosenfeld <326774742@mby.co.il>
2025-03-04 21:14:35 +0200

    project almost fixed

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace DTO
 {
     //public record userDTO( int? UserId, [EmailAddress(ErrorMessage = "כתובת מייל לא תקינה"), Required] string Email, [Required] string Password, string? FirstName, string? LastName);
-    public record userRegisterDTO([EmailAddress(ErrorMessage = "כתובת מייל לא תקינה"), Required] string Email,[Required] string Password,  string FirstName, string LastName);
+    public record userRegisterDTO([EmailAddress(ErrorMessage = "כתובת מייל לא תקינה"), Required] string Email,[Required] string Password,  string FirstName, [StringLength(20, ErrorMessage = "Name can be between 2 till 20 letters", MinimumLength = 2), Required] string LastName);
     //public record userRegisterWithOutPasswordDTO([EmailAddress(ErrorMessage = "כתובת מייל לא תקינה"), Required] string Email,  string? FirstName,  string? LastName);
     public record userIdDTO(int? UserId);
     public record userLoginIdDTO([EmailAddress(ErrorMessage = "כתובת מייל לא תקינה"), Required] string Email,[Required] string Password);
